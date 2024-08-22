ab296717cb9c9f4fd33c1e8ae38aa0a7decedf72
humeyraaltuntas <humeyraaltuntas34@gmail.com>
2024-08-22 21:35:14 +0300

    KdvHesap created

--- src/KdvHesap.java
+++ src/KdvHesap.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class KdvHesap {
     public static void main(String[] args) {
+        double tutar,kdvOran = 0.22,kdvTutar,kdvliTutar;
+
+        Scanner hum = new Scanner(System.in);
+        System.out.println("ücret tutarını giriniz : " );
+        tutar = hum .nextDouble();
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz Tutar : "+tutar);
+        System.out.println("KDV Oranı: "+kdvOran);
+        System.out.println("KDV Tutarı: "+kdvTutar);
+        System.out.println("KDVli Tutarı: "+kdvliTutar);
+
+        
+
+
+
+
 
     }
 }
